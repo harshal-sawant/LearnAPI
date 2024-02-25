772bc62a353b3d95772506db761661b72e027203
harshal <harshalsawant2@gmail.com>
2024-02-25 23:09:56 +0530

    Add project files.

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,63 @@
+﻿using LearnAPI.Modal;
+using LearnAPI.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
+
+namespace LearnAPI.Controllers
+{
+    //[EnableRateLimiting("fixedwindow")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomerService service;
+        public CustomerController(ICustomerService service) {
+            this.service = service;
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> GetAll()
+        {
+            var data= await this.service.GetAll();
+            if(data == null )
+            {
+                return NotFound();
+            }
+            return Ok(data);
+        }
+
+        //[DisableRateLimiting]
+        [HttpGet("GetByCode")]
+        public async Task<IActionResult> GetByCode(string code)
+        {
+            var data = await this.service.GetByCode(code);
+            if (data == null)
+            {
+                return NotFound();
+            }
+            return Ok(data);
+        }
+
+        [HttpPost("Create")]
+        public async Task<IActionResult> Create(CustomerModal _data)
+        {
+            var data = await this.service.Create(_data);
+                        return Ok(data);
+        }
+
+        [HttpPost("Update")]
+        public async Task<IActionResult> Update(CustomerModal _data, string code)
+        {
+            var data = await this.service.Update(_data, code);
+            return Ok(data);
+        }
+
+        [HttpDelete("Remove")]
+        public async Task<IActionResult> Remove(string code)
+        {
+            var data = await this.service.Remove(code);
+            return Ok(data);
+        }
+    }
+}
---
+++ Service/ICustomerService.cs
@@ -0,0 +1,21 @@
+﻿using LearnAPI.Helper;
+using LearnAPI.Modal;
+using LearnAPI.Repos.Models;
+
+namespace LearnAPI.Service
+{
+    public interface ICustomerService
+    {
+          Task<List<CustomerModal>> GetAll();
+
+          Task<CustomerModal> GetByCode(string code);
+
+        Task<APIResponse> Remove(string code);
+
+        Task<APIResponse> Create(CustomerModal data);
+
+        Task<APIResponse> Update(CustomerModal data, string code);
+
+
+    }
+}
