02ea306a6b920b670cefaf1b399116f4b727ff72
harshal <harshalsawant2@gmail.com>
2024-03-03 18:17:48 +0530

    added image upload

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,123 @@
+﻿using LearnAPI.Helper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
+
+namespace LearnAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IWebHostEnvironment environment;
+        public ProductController(IWebHostEnvironment environment)
+        {
+            this.environment = environment;
+        }
+
+        [HttpPut("UploadImage")]
+        public async Task<IActionResult> UploadImage(IFormFile formFile, string productcode)
+        {
+            APIResponse response= new APIResponse();
+            try
+            {
+                string Filepath = GetFilepath(productcode);
+                if(!System.IO.Directory.Exists(Filepath))
+                {
+                    System.IO.Directory.CreateDirectory(Filepath);
+                }
+                string imagepath = Filepath + "\\" + productcode + ".png";
+                if(!System.IO.File.Exists(imagepath))
+                {
+                    System.IO.File.Delete(imagepath);
+                }
+                using (FileStream stream = System.IO.File.Create(imagepath))
+                {
+                    await formFile.CopyToAsync(stream);
+                    response.ResponseCode= 200;
+                    response.Result = "pass";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                response.ErrorMessage= ex.Message;
+            }
+            return Ok(response);
+        }
+
+        [HttpPut("MultiUploadImage")]
+        public async Task<IActionResult> MultiUploadImage(IFormFileCollection filecollection, string productcode)
+        {
+            APIResponse response = new APIResponse();
+            int passcount = 0; int errorcount =0;
+            try
+            {
+                string Filepath = GetFilepath(productcode);
+                if (!System.IO.Directory.Exists(Filepath))
+                {
+                    System.IO.Directory.CreateDirectory(Filepath);
+                }
+
+                foreach(var file in filecollection)
+                {
+                    string imagepath = Filepath + "\\" + file.FileName;
+                    if (!System.IO.File.Exists(imagepath))
+                    {
+                        System.IO.File.Delete(imagepath);
+                    }
+                    using (FileStream stream = System.IO.File.Create(imagepath))
+                    {
+                        await file.CopyToAsync(stream);
+                      passcount++;
+                    }
+                }
+
+                                }
+            catch (Exception ex)
+            {
+                errorcount++;
+
+                response.ErrorMessage = ex.Message;
+            }
+
+            response.ResponseCode=200;
+            response.Result = passcount + " Files uploaded & " + errorcount + " Files failed";
+
+            return Ok(response);
+        }
+
+
+        [HttpGet("GetImage")]
+        public async Task<IActionResult> GetImage(string productcode)
+        {
+            string Imageurl = string.Empty;
+            string hosturl = $"{this.Request.Scheme}://{this.Request.Host}{this.Request.PathBase}";
+
+            try
+            {
+                string Filepath = GetFilepath(productcode);
+                string imagepath = Filepath + "\\" + productcode + ".png";
+                if(System.IO.File.Exists(imagepath))
+                {
+                    Imageurl = hosturl + "/Upload/product/" + productcode + "/" + productcode + ".png";
+                } else
+                {
+                    return NotFound();
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+            return Ok(Imageurl);
+        }
+
+        [NonAction]
+        private string GetFilepath(string productcode)
+        {
+            return this.environment.WebRootPath + "\\Upload\\product\\" + productcode;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -88,6 +88,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseStaticFiles();
+
 app.UseCors();
 
 app.UseHttpsRedirection();
