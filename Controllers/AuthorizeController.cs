fe51d2edbb2a835eb37d590a34a6abf0ac7c3061
harshal <harshalsawant2@gmail.com>
2024-03-03 17:25:57 +0530

    added token refresh logic

--- Controllers/AuthorizeController.cs
+++ Controllers/AuthorizeController.cs
@@ -57,5 +57,78 @@ namespace LearnAPI.Controllers
             }
                
         }
+
+        [HttpPost("GenerateRefreshToken")]
+        public async Task<IActionResult> GenerateToken([FromBody] TokenResponse token)
+        {
+            var _refreshtoken = await this.context.TblRefreshtokens.FirstOrDefaultAsync(item => item.Refreshtoken == token.RefreshToken);
+            if (_refreshtoken != null)
+            {
+                var tokenhandler = new JwtSecurityTokenHandler();
+                var tokenkey = Encoding.UTF8.GetBytes(this.jwtSettings.securitykey);
+                SecurityToken securityToken;
+                var principal = tokenhandler.ValidateToken(token.Token, new TokenValidationParameters()
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(tokenkey),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                }, out securityToken);
+
+                var _token = securityToken as JwtSecurityToken;
+                if(_token != null && _token.Header.Alg.Equals(SecurityAlgorithms.HmacSha256))
+                {
+                    string username = principal.Identity?.Name;
+                    var _existdata = await this.context.TblRefreshtokens.FirstOrDefaultAsync(item => item.Userid == username && item.Refreshtoken == token.RefreshToken);
+                    if(_existdata != null)
+                    {
+                        var _newtoken = new JwtSecurityToken(
+                            claims: principal.Claims.ToArray(),
+                            expires: DateTime.Now.AddSeconds(30),
+                            signingCredentials: new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(this.jwtSettings.securitykey)), SecurityAlgorithms.HmacSha256)
+                            );
+
+                        var _finaltoken = tokenhandler.WriteToken(_newtoken);
+                        return Ok(new TokenResponse()
+                        {
+                            Token = _finaltoken,
+                            RefreshToken = await this.refresh.GenerateToken(username)
+                        });
+
+                    }
+                    else
+                    {
+                        return Unauthorized();
+                    }
+                } else
+                {
+                    return Unauthorized();
+                }
+
+                //var tokendesc = new SecurityTokenDescriptor
+                //{
+                //    Subject = new ClaimsIdentity(new Claim[]
+                //    {
+                //        new Claim(ClaimTypes.Name, user.Code),
+                //        new Claim(ClaimTypes.Role, user.Role),
+                //    }),
+                //    Expires = DateTime.UtcNow.AddSeconds(30),
+                //    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(tokenkey), SecurityAlgorithms.HmacSha256)
+                //};
+                //var token = tokenhandler.CreateToken(tokendesc);
+                //var finaltoken = tokenhandler.WriteToken(token);
+                //return Ok(new TokenResponse()
+                //{
+                //    Token = finaltoken,
+                //    RefreshToken = await this.refresh.GenerateToken(userCred.Username)
+                //});
+
+            }
+            else
+            {
+                return Unauthorized();
+            }
+
+        }
     }
 }
